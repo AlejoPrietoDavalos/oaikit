d51dc41be8fbd0c9a488baf6c0ac2f0018e3f1e1
AlejoPrietoDavalos <alejoprietodavalos@gmail.com>
2024-07-27 13:07:54 -0300

    OAIModels GPT

--- oaikit/__init__.py
+++ oaikit/__init__.py
@@ -1,6 +1,6 @@
 from oaikit.oai import OAI
 from oaikit.msg import OAIMsg
-from oaikit.models import OAIModels
+from oaikit.models import OAIModels, T_OAIModels, GPT_4O, GPT_4O_MINI
 from oaikit.role import SYSTEM, USER, ASSISTANT, OAIRole
 from oaikit.content.text import ContentText
 from oaikit.content.image import ContentImage
