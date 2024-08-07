f38155fa54a4171891e1b5c4c5988f17ef7535ba
AlejoPrietoDavalos <alejoprietodavalos@gmail.com>
2024-08-07 15:59:40 -0300

    0.2.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name = "oaikit",
-    version = "0.2.0",
+    version = "0.2.1",
     author = "Alejo Prieto Dávalos",
     author_email = "alejoprietodavalos@gmail.com",
     packages = find_packages(),
