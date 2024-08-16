72e18a255d0b8a55dc2f9c997554f0d8835d9a25
AlejoPrietoDavalos <alejoprietodavalos@gmail.com>
2024-08-16 19:13:37 -0300

    0.2.9: T_OAI

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="oaikit",
-    version="0.2.8",
+    version="0.2.9",
     author="Alejo Prieto Dávalos",
     author_email="alejoprietodavalos@gmail.com",
     packages=find_packages(),
